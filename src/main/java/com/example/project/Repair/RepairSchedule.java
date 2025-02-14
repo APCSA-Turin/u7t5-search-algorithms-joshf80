831835a77af48c0fdf6626d126f2059ca2e29dab
Joshua <139407249+joshf80@users.noreply.github.com>
2025-02-14 14:30:25 +0000

    final

--- src/main/java/com/example/project/Repair/RepairSchedule.java
+++ src/main/java/com/example/project/Repair/RepairSchedule.java
@@ -12,7 +12,8 @@ public class RepairSchedule {
      * Precondition: n >= 0
      */
     public RepairSchedule(int n) {
-        /* to be implemented in part (a) */
+        numberOfMechanics = n;
+        schedule = new ArrayList<CarRepair>();
     }
 
     public ArrayList<CarRepair> getSchedule() {
@@ -23,6 +24,12 @@ public class RepairSchedule {
      * Precondition: 0 <= m < numberOfMechanics and b >= 0
      */
     public boolean addRepair(int m, int b) {
+        for (CarRepair carRepair : schedule) {
+            if (carRepair.getBayNum() == b || carRepair.getMechanicNum() == m) {
+                return false;
+            }
+        }
+        schedule.add(new CarRepair(m, b));
         return true;
     }
 
@@ -30,7 +37,17 @@ public class RepairSchedule {
      * as described in part (b).
      */
     public ArrayList<Integer> availableMechanics() {
-        return new ArrayList<Integer>();
+        ArrayList<Integer> available = new ArrayList<Integer>();
+        
+        for (int i = 0; i < numberOfMechanics; i++) {
+            available.add(i);
+        }
+        
+        for (int i = 0; i < schedule.size(); i++) {
+            available.remove(available.indexOf(schedule.get(i).getMechanicNum()));
+        }
+
+        return available;
     }
 
     /** Removes an element from schedule when a repair is complete.
