842116524e0241b8ef54dc074882d77ed4707438
Joshua <139407249+joshf80@users.noreply.github.com>
2025-02-23 20:38:22 +0000

    finished

--- src/main/java/com/example/project/Binary_Search/BinarySearch.java
+++ src/main/java/com/example/project/Binary_Search/BinarySearch.java
@@ -4,21 +4,34 @@ public class BinarySearch {
     // implements binary search on the elements list for target,
     // and returns the index at which the target is found, or -1 if
     public static int binarySearch(int[] elements, int target) {
-        // int loopCounter = 0; // for testing
-        // int leftIdx =    // TODO: assign this initial value
-        // int rightIdx =   // TODO: assign this initial value
+        int loopCounter = 0; // for testing
+        int leftIdx =  0; 
+        int rightIdx = elements.length - 1; 
 
-        // while (    ) {  // TODO: determine this condition (hint: see slides 61-63)
-        //     loopCounter++; // for testing
-        //     System.out.print(loopCounter + " "); // for testing
-        //     int middleIdx =   // TODO: determine what this should be
+        // elements index = {0};
+        // int[] elements = {5};
+        // int target = 5;
+        // int expectedIndex = 0; 
 
-        //     // TODO: write the rest of the code to compare middleIdx to the target
-        //     //  and adjust leftIdx and rightIdx as appropriate (see slides if needed)
+        while (leftIdx <= rightIdx) {
+            loopCounter++; // for testing
+            System.out.print(loopCounter + " "); // for testing
+
+            int middleIdx = (leftIdx + rightIdx) / 2;
+
+            if (elements[middleIdx] == target) {
+                return middleIdx;
+            } else {
+                if (elements[middleIdx] > target) {
+                    rightIdx = middleIdx - 1;
+                }
+                else if (elements[middleIdx] < target) {
+                    leftIdx = middleIdx + 1;
+                }
+            }
 
 
-
-        // }
+        }
         return -1; // not found
     }
 }
--- src/main/java/com/example/project/Spell_Checker/SpellChecker.java
+++ src/main/java/com/example/project/Spell_Checker/SpellChecker.java
@@ -50,7 +50,29 @@ public class SpellChecker {
      *  prints that value out before returning.
      */
     public boolean binarySpellCheck(String word) {
-        return false;
+        int leftIdx =  0; 
+        int rightIdx = dictionary.size() - 1; 
+        loopCounter = 0; // for testing
+
+
+        while (leftIdx <= rightIdx) {
+            loopCounter++; // for testing
+            int middleIdx = (leftIdx + rightIdx) / 2;
+
+            if (dictionary.get(middleIdx).equals(word)) {
+                System.out.println("-- BINARY SEARCH: Number of words checked (loop iterations): " + loopCounter);
+                return true;
+            } else {
+                if (dictionary.get(middleIdx).compareTo(word) > 0) {
+                    rightIdx = middleIdx - 1;
+                }
+                else if (dictionary.get(middleIdx).compareTo(word) < 0) {
+                    leftIdx = middleIdx + 1;
+                }
+            }
+        }
+        System.out.println("-- BINARY SEARCH: Number of words checked (loop iterations): " + loopCounter);
+        return false; // not found
     }
 
     // private helper method, called in the constructor, which loads the words
